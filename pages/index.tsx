fa01df00506c221bfe347f5ef998a0d81de45a67
Aliff Danial <alfdnl080@gmail.com>
2025-01-18 15:14:13 +0800

    remove unused stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   const handleShare = async () => {
