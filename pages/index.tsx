1ccbb68fb20b0e6e459e23ee5f249dce44f560d9
Aliff Danial <alfdnl080@gmail.com>
2025-01-18 16:10:11 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home() {
 
     // Simulate Instagram Story sharing (Instagram won't accept this directly, but you can test)
     console.log(objectURL);
-    window.location.href = `barcelona://create?text=text%20habibi&url=${objectURL}`;
+    window.location.href = `www.kawenlah.com`;
   };
   return (
     <div className={`flex items-center justify-center  min-h-screen `}>
